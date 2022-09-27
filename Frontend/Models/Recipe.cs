83254c54ad799602b2fd80350c6d84e8f5864518
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-09-27 22:38:34 +0200

    ✅ DONE: Recipes

---
+++ Frontend/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Frontend.Models;
+
+public class Recipe
+{
+    public Guid Id { get; set; } = new();
+    public string Name { get; set; } = string.Empty;
+    public List<string> Ingredients { get; set; } = new();
+    public List<string> Instructions { get; set; } = new();
+    public List<Guid> CategoriesIds { get; set; } = new();
+}
+
